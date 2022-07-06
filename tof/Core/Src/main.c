2a2d78af15b4d55a812d54432c6fb169136745c5
ilovethedogs <79030282+ilovethedogs@users.noreply.github.com>
2022-07-06 20:56:25 +0900

    working on multiple tofs

--- tof/Core/Src/main.c
+++ tof/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "VL53L1X_api.h"
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -44,7 +45,17 @@ I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+uint8_t devs[9] = {
+		0x52,
+		0x54,
+		0x56,
+		0x58,
+		0x5A,
+		0x5C,
+		0x5E,
+		0x60,
+		0x62
+};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -53,12 +64,16 @@ static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
-
+void TurnOffAll(void);
+uint8_t TurnOnAt(uint8_t i);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+int _write(int file, char* p, int len) {
+	HAL_UART_Transmit(&huart2, p, len, 16);
+	return len;
+}
 /* USER CODE END 0 */
 
 /**
@@ -102,7 +117,8 @@ int main(void)
   }
 
   VL53L1X_SensorInit(dev);
-  VL53L1X_StartRanging(dev);
+  //VL53L1X_SetI2CAddress(dev, dev1);
+  VL53L1X_StartRanging(dev1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -110,15 +126,20 @@ int main(void)
   uint8_t dataReady = 0;
   uint8_t getting[6] = {71, 69, 84, 83, 10, 0};
   uint8_t data[6] = {68, 65, 84, 65, 10, 0};
+  uint16_t dis = 0;
   while (1)
   {
 	  while (dataReady == 0){
-		  VL53L1X_CheckForDataReady(dev, &dataReady);
-		  HAL_UART_Transmit(&huart2, getting, 6, 10);
+		  VL53L1X_CheckForDataReady(dev1, &dataReady);
+		  //HAL_UART_Transmit(&huart2, getting, 6, 10);
 		  HAL_Delay(2);
 	  }
-	  HAL_UART_Transmit(&huart2, data, 6, 10);
+	  VL53L1X_GetDistance(dev1, &dis);
+	  VL53L1X_ClearInterrupt(dev1);
+	  //HAL_UART_Transmit(&huart2, data, 6, 10);
+	  printf("%d\n", dis);
 	  dataReady = 0;
+	  HAL_Delay(50);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -253,22 +274,106 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin : PB8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13|GPIO_PIN_2|GPIO_PIN_10|GPIO_PIN_11
+                          |GPIO_PIN_12, GPIO_PIN_RESET);
 
-  /* EXTI interrupt init*/
-  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PC13 PC2 PC10 PC11
+                           PC12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_2|GPIO_PIN_10|GPIO_PIN_11
+                          |GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA13 PA14 PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PD2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
 }
 
 /* USER CODE BEGIN 4 */
+void TurnOffAll(void) {
+	  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_RESET);
 
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_10, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12, GPIO_PIN_RESET);
+
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET);
+}
+
+uint8_t TurnOnAt(uint8_t i) {
+	uint8_t bootState = 0;
+	uint8_t boot[7] = {66, 79, 79, 84, 48, 10, 0};
+	switch (i) {
+	case 0:
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_SET);
+		while(bootState==0){
+			VL53L1X_BootState(dev[0], &sensorState);
+			HAL_UART_Transmit(&huart2, boot, 7, 10);
+			HAL_Delay(2);
+		}
+		break;
+	case 1:
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+		boot[4] = 49;
+		while(bootState==0){
+			VL53L1X_BootState(dev[1], &sensorState);
+			HAL_UART_Transmit(&huart2, boot, 7, 10);
+			HAL_Delay(2);
+		}
+		break;
+	case 2:
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_10, GPIO_PIN_RESET);
+		break;
+	case 4:
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_11, GPIO_PIN_RESET);
+		break;
+	case 5:
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12, GPIO_PIN_RESET);
+		break;
+	case 6:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_RESET);
+		break;
+	case 7:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14, GPIO_PIN_RESET);
+		break;
+	case 8:
+		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15, GPIO_PIN_RESET);
+		break;
+	default:
+		// shit
+	}
+	return bootState;
+}
 /* USER CODE END 4 */
 
 /**
